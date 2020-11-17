02705ca2526fd72b224680603937e6886bf88ace
blavaud <blavaud@ugc.fr>
2020-11-17 22:05:57 +0100

    Program page

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace MoreOrLessGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Initialize Game
+            Console.WriteLine("New More or less Game !!!");
+            Console.WriteLine("Try to discover THE SECRET NUMBER by entering a number between 0 and 100 include !");
+
+            // set Secret Number
+            Random random = new Random();
+            int secretNumber = random.Next(100);
+
+            Console.WriteLine("Secret number chosen! You can play ! {0}", secretNumber);
+
+            int playerNumber;
+            // Game loop
+            do
+            {
+                string playerEntry = Console.ReadLine();
+
+                if (!int.TryParse(playerEntry, out playerNumber))
+                {
+                    Console.WriteLine("The secret number is a NUMBER! ");
+                }
+                else
+                {
+                    if (playerNumber < 0 || playerNumber >= 100)
+                    {
+                        Console.WriteLine("The Secret number is between 0 and 100 included.");
+                    }
+                    else
+                    {
+                        if (playerNumber == secretNumber)
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            if (secretNumber > playerNumber)
+                            {
+                                Console.WriteLine("TRY AGAIN, the secret number is bigger.");
+                            }
+                            else
+                            {
+                                Console.WriteLine("TRY AGAIN, the secret number is smaller.");
+                            }
+                        }
+                    }
+                }
+            } while (playerNumber != secretNumber);
+
+            Console.WriteLine("YOU WON!!! You can... Code again!");
+        }
+
+        public static void RunGame()
+        {
+        }
+    }
+}
